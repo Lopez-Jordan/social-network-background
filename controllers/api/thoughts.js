dcf37e19cee4bc5647e664d8cb54a27203edeeff
Jordan Lopez <jordanlopezemail@gmail.com>
2023-08-24 00:42:52 -0700

    added a bunch of pseudo code

--- controllers/api/thoughts.js
+++ controllers/api/thoughts.js
@@ -1,2 +1,17 @@
-
 // thought routes go here
+const router = require('express').Router();
+
+// Get all thoughts '/'     //  /api/thoughts/
+
+//  Get thought by _id ':id'        //    /api/thoughts/:id
+
+//  Post create new thought '/'    //      /api/thoughts/
+
+//  Put update a thought by _id  ':id'   //      /api/thoughts/:id
+
+//  Delete a thought by _id   ':id'      //      /api/thoughts/:id
+
+// Post add new reaction to `thoughtId`   '/:thoughtId/reactions'
+
+// Delete reaction through reactionId to `thoughtId`   '/:thoughtId/reactions'
+module.exports = router;
--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,1 +1,17 @@
 // user routes go here
+const router = require('express').Router();
+
+// Get All Users    '/'          //  /api/users/
+
+// Get single user by Id '/:id'     //  /api/users/:id
+
+// Post create new user    '/'             //  /api/users
+
+// Put update user by _id   '/:id'       //  /api/users/:id
+
+// Delete user by _id   '/:id'       //  /api/users/:id
+
+// Post add new friend by `friendId` to `userId` friends list '/:userId/friends/:friendId'
+
+// Delete friend by `friendId` to `userId` friends list '/:userId/friends/:friendId'
+module.exports = router;
