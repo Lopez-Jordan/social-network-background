eafac27eae5777d20d8d33a1c97937667bc4df3b
Jordan Lopez <jordanlopezemail@gmail.com>
2023-08-24 20:29:22 -0700

    made the user model

--- controllers/api/thoughts.js
+++ controllers/api/thoughts.js
@@ -3,15 +3,15 @@ const router = require('express').Router();
 
 // Get all thoughts '/'     //  /api/thoughts/
 
-//  Get thought by _id ':id'        //    /api/thoughts/:id
+//  Get thought by _id '/:id'        //    /api/thoughts/:id
 
 //  Post create new thought '/'    //      /api/thoughts/
 
-//  Put update a thought by _id  ':id'   //      /api/thoughts/:id
+//  Put update a thought by _id  '/:id'   //      /api/thoughts/:id
 
-//  Delete a thought by _id   ':id'      //      /api/thoughts/:id
+//  Delete a thought by _id   '/:id'      //      /api/thoughts/:id
 
-// Post add new reaction to `thoughtId`   '/:thoughtId/reactions'
+// Post create new reaction to `thoughtId`   '/:thoughtId/reactions'
 
-// Delete reaction through reactionId to `thoughtId`   '/:thoughtId/reactions'
+// Delete reaction through reactionId to `thoughtId`   '/:thoughtId/reactions/reactionId'
 module.exports = router;
